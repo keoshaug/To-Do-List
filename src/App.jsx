e734ac81ffa1caf7d9e527cd98e069fe3767396c
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-08-08 13:28:28 -0400

    adding delete todo function

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,12 @@ export default function App () {
         )
   }
 
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
 
   return (
   <>
@@ -56,7 +62,7 @@ export default function App () {
                 onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                {todo.title}
              </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={()=> deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
           </li>
         )
     })}
